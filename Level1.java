b70f188d3f8bf017aa4dd87698990bdae409d334
pranavipathuri <pp42496@srcs.org>
2021-09-23 16:52:42 -0500

    part of 3 completed

--- Level1.java
+++ Level1.java
@@ -25,23 +25,11 @@ public class Level1 extends World
      */
     private void prepare()
     {
-        Door door = new Door();
-        addObject(door,667,40);
-        door.setLocation(677,48);
-        door.setLocation(682,44);
+        Floor floor = new Floor();
+        addObject(new Floor(), 600, 800);
         Player player = new Player();
-        addObject(player,21,570);
-        door.setLocation(640,55);
-        door.setLocation(677,51);
-        door.setLocation(658,24);
-        door.setLocation(970,132);
-        door.setLocation(1176,26);
-        door.setLocation(1153,40);
-        door.setLocation(1164,40);
-        door.setLocation(1180,30);
-        door.setLocation(1176,56);
-        door.setLocation(1180,14);
-        door.setLocation(1164,46);
-        player.setLocation(20,661);
+        addObject(player,38,752);
+        Door door = new Door();
+        addObject(door,1174,40);
     }
 }
