926f0bd28de8719ed1e1a13de702f71edec33248
pranavipathuri <pp42496@srcs.org>
2021-10-07 17:39:53 -0500

    activity 4 ALMOST DONE

--- Player.java
+++ Player.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Player here.
- * 
- * @Pranavi Pathuri 
- * @version (a version number or a date)
- */
 public class Player extends Actor
 {
     private Health[] health;
@@ -25,12 +19,51 @@ public class Player extends Actor
     private Class NEXT_LEVEL;
     private GreenfootSound MUSIC;
     
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
+    public Player(int speed, float jumpForce, float gravity, int maxHealth, int maxPowerUp, Class nextLevel, GreenfootSound muisc)
     {
-        // Add your action code here.
+        WALK_ANIMATION = new GreenfootImage[]
+                    {
+                        new GreenfootImage("walk0.png"),
+                        new GreenfootImage("walk1.png"),
+                        new GreenfootImage("walk2.png"),
+                        new GreenfootImage("walk3.png"),
+                        new GreenfootImage("walk4.png"),
+                        new GreenfootImage("walk5.png"),
+                        
+                    };
+    }
+    
+    public void act()
+    {   
+    }
+    
+    public void addedToWorld(World world) {}
+    
+    private void animator()
+    {
+        if(frame % (15 - 2 * speed) == 0)
+        {
+          if(walkIndex < WALK_ANIMATION.length)
+          {
+              setImae(WALK_ANIMATION[walkIndex]);
+              walkIndex++l;
+          }
+          else
+          {
+              walkIndex = 0;
+          }
+        }
+        frame++;
+    }
+    
+    private void walk() {}
+    private void jump() {}
+    private void fall() {}
+    private void onCollision() {}
+    private void mirrorImages() {}
+    private void gameOver() {}
+    private boolean isOnGround()
+    {
+        return false;
     }
 }
