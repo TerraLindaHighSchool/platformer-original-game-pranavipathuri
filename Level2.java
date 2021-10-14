988c0559bf3bbc4c6ccf2a01214b265f420d0d89
pranavipathuri <89489635+pranavipathuri@users.noreply.github.com>
2021-10-14 00:05:27 -0700

    activity 7.6 done

--- Level2.java
+++ Level2.java
@@ -8,13 +8,63 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Level2 extends World
 {
-    /**
+    private final float GRAVITY = 0.0677f;
+    private final GreenfootSound MUSIC = new GreenfootSound("incompetech_tribal.mp3");
+    private final int SPEED = 4;
+    private final float JUMP_FORCE = 5.6f;
+    private final int MAX_HEALTH = 5;
+    private final int MAX_POWERUP = 3;
+    private final Class NEXT_LEVEL = WinSplash.class;
+     /**
      * Constructor for objects of class BrickWorld.
      * 
      */
     public Level2()
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
-        super(1200, 800, 1); 
+        super(1200, 800, 1, false); 
+        prepare();
+    }
+    
+    public void act()
+    {
+        spawn();
+    }
+
+    private void prepare()
+    {
+        setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
+            Door.class, HUD.class);
+
+        Player player = new Player(SPEED, JUMP_FORCE, GRAVITY, 
+                MAX_HEALTH, MAX_POWERUP, NEXT_LEVEL, MUSIC);
+
+        addObject(player,47,733);
+        addObject(new Floor(), 600, 800);
+        addObject(new Door(),1174,40); 
+        addObject(new Bomb(GRAVITY),367,665);
+        addObject(new Bomb(GRAVITY), 455, 665);
+        
+        addObject(new BrickWall(),876,434);
+        addObject(new BrickWall(),960,101);
+        addObject(new BrickWall(),400,710);
+
+        addObject(new Gem(),738,576);
+
+        addObject(new SmBrickWall(),608,103);
+
+        addObject(new SmBrickWall(),690,608);
+        addObject(new SmBrickWall(),584,528);
+
+        addObject(new SmBrickWall(),657,347);
+        addObject(new SmBrickWall(),548,245);
+    }
+
+     private void spawn()
+    {
+        if(Math.random() < 0.0025)
+        {
+            addObject(new Rock(GRAVITY), Greenfoot.getRandomNumber(1200), -30);
+        }
     }
 }
